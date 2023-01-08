fbc1eb00955f726e295d67490a3b95392a6be7a4
tomoya <t.trance0208@gmail.com>
2023-01-08 18:36:11 +0900

    eclipse & edit lesson4

---
+++ lesson4/src/lesson4/Chapter04.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1 = "tomoya";
+		System.out.println(str1);
+		str1 = "sample text";
+		System.out.println(str1);
+		final int LUK_NUM = 7;
+		System.out.println(LUK_NUM);
+	}
+}
